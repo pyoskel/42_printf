04a62ad906f1ea308b4b99b95fae42fa1f9d5952
Pascal Bartocha <pabartoc@3-F-10.42heilbronn.de>
2025-10-08 20:47:13 +0200

    create: printf.c

---
+++ ft_printf.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 01:24:04 by pabartoc          #+#    #+#             */
+/*   Updated: 2025/10/08 20:26:00 by pabartoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	int		printed;
+	va_list	args;
+
+	va_start(args, format);
+	va_end;
+
+}
