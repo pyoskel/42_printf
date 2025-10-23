60d0925505e3a0075ec8cc6bc3d9496a1ee97943
pabartoc <pasigermany@gmail.com>
2025-10-23 06:30:49 +0200

    added: a condition  to work after der % sign, named; handle_specifier

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 01:24:04 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/23 01:45:46 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/10/23 06:24:30 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	ft_printf(const char *input, ...)
 {
-	int		printed;
+	int		i;
+	int		counter;
 	va_list	args;
 
 	va_start(args, input);
-	return (va_end(args), input);
+	i = -1;
+	counter = 0;
+	while (input != '\0')
+	{
+		if (input[++i] == '%')
+			counter += handle_specifier(input[++1], args);
+		
+	}
+	
+	return (va_end(args), counter);
 }
