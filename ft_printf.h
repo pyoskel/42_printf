69666c4e5d1b0ff3cdea8991e620992538ff0950
pabartoc <pabartoc@student.42heilbronn.de>
2025-10-25 01:10:26 +0200

    added: the library  (printf) for testing my functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 02:30:02 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/08 20:27:28 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/10/24 01:54:36 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdio.h>  //for printf function
 # include <stdarg.h> //for variadic functions
 
 int	ft_printf(const char *format, ...);
