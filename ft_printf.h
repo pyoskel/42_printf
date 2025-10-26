1fb4cd45a87672868934acd290f860b36e034638
pabartoc <pabartoc@student.42heilbronn.de>
2025-10-26 16:54:37 +0100

    addad : and implemented a makro for the hexalower and upper, also for a base10. and define the system (linux or MacOS)

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 02:30:02 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/24 01:54:36 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/10/26 16:51:17 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,21 @@
 # include <stdio.h>  //for printf function
 # include <stdarg.h> //for variadic functions
 
+# define HEXALOWER "0123456789abcdef"
+# define HEXAUPPER "0123456789ABCDEF"
+# define BASE10 "0123456789"
+
+# if defined(__linux__)
+#  define PTRNULL "(nil)"
+#  define PTRSIZE 5
+# elif defined(__APPLE__)
+#  define PTRNULL "0x0"
+#  define PTRSIZE 3
+# endif
+
 int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putptr(void *ptr, int flag);
 
 #endif
