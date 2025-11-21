119ef97640b67018aac4f99067094b24e6904701
pabartoc <pabartoc@student.42heilbronn.de>
2025-11-21 20:43:37 +0100

    fix for macOS: update handle_specifier to use pointer for va_list argument

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,28 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 01:24:04 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/11/18 21:25:36 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/11/21 20:37:11 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	handle_specifier(char specifier, va_list args)
+int	handle_specifier(char specifier, va_list *args)
 {
 	int	counter;
 
 	if (specifier == 'c')
-		counter = ft_putchar(va_arg(args, int));
+		counter = ft_putchar(va_arg(*args, int));
 	else if (specifier == 's')
-		counter = ft_putstr(va_arg(args, char *));
+		counter = ft_putstr(va_arg(*args, char *));
 	else if (specifier == 'p')
-		counter = ft_putptr(va_arg(args, void *), 0);
+		counter = ft_putptr(va_arg(*args, void *), 0);
 	else if (specifier == 'd' || specifier == 'i')
-		counter = ft_putnbr(va_arg(args, int), 10);
+		counter = ft_putnbr(va_arg(*args, int), 10);
 	else if (specifier == 'u')
-		counter = ft_puthex_or_u(va_arg(args, unsigned int), specifier, 10);
+		counter = ft_puthex_or_u(va_arg(*args, unsigned int), specifier, 10);
 	else if (specifier == 'x' || specifier == 'X')
-		counter = ft_puthex_or_u(va_arg(args, unsigned int), specifier, 16);
+		counter = ft_puthex_or_u(va_arg(*args, unsigned int), specifier, 16);
 	else if (specifier == '%')
 		counter = write(1, "%", 1);
 	else
@@ -49,7 +49,7 @@ int	ft_printf(const char *input, ...)
 	{
 		if (input[i] == '%')
 		{
-			write_check = handle_specifier(input[++i], args);
+			write_check = handle_specifier(input[++i], &args);
 			if (write_check == -1)
 				return (va_end(args), -1);
 			counter += write_check;
