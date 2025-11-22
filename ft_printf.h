ac77f3a52616fa0c5a26be568e8e188b605395e3
pabartoc <pabartoc@student.42heilbronn.de>
2025-11-22 01:40:06 +0100

    updated: comment out unused include for printf function in ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 02:30:02 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/30 05:03:32 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/11/22 01:30:27 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdio.h>  //for printf function
+// # include <stdio.h>  //for printf function
 # include <stdarg.h> //for variadic functions
 # include <unistd.h> //for write function
 # include <limits.h> //for INT_MIN 
